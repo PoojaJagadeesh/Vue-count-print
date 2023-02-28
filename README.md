2bcf4c63590e8b5cb8e0bffa728df231c5a701a7
PoojaJagadeesh <poojajagadeeshan@gmail.com>
2023-02-28 14:51:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue-count-print
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-lz1agm)
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+  <div>
+    <p v-for="i in count">{{ i }}</p>
+    <button @click="add">Click</button>
+    <div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      i: 0,
+      count:0,
+    };
+  },
+  methods:{
+    add(){
+     this.count++;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
